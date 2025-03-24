5118bd89fac1c731b94f03401638f4129fda6a36
Baizidmdashadzzaman <baizid.md.ashadzzaman@gmail.com>
2025-03-24 12:14:44 +0600

    03-24-2023

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // src/index.js
 import DeveloperPage from './react/DeveloperPage';
+import DeveloperPageAsad from './react/DeveloperPageAsad';
+
 import DeveloperPlugin from './vue/DeveloperPlugin';
 import DeveloperComponent from './vue/DeveloperComponent.vue';
+import DeveloperComponentAsad from './vue/DeveloperComponentAsad.vue';
 
-export { DeveloperPage, DeveloperPlugin, DeveloperComponent };
+export { DeveloperPage,DeveloperPageAsad,DeveloperPlugin,DeveloperComponent,DeveloperComponentAsad };
