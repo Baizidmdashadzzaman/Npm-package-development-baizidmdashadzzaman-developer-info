b9ec0f81ffad2720291cf98a82718cda928389e0
Baizidmdashadzzaman <baizid.md.ashadzzaman@gmail.com>
2025-03-24 19:12:49 +0600

    updated Developer component page in vue js

--- src/vue/DeveloperComponent.vue
+++ src/vue/DeveloperComponent.vue
@@ -1,19 +1,19 @@
 <template>
     <div style="padding: 20px;">
       <h1>System Developer Information:</h1>
-      <p><strong>Developer name:</strong> {{ name??'Baizid MD Ashadzzaman' }}</p>
-      <p><strong>Email:</strong> {{ email??'baizid.md.ashadzzaman@gmail.com' }}</p>
-      <p>
+      <p v-if="name"><strong>Developer name:</strong> {{ name }}</p>
+      <p v-if="email"><strong>Email:</strong> {{ email }}</p>
+      <p v-if="github">
         <strong>GitHub:</strong>
-        <a :href="github??'https://github.com/Baizidmdashadzzaman'" target="_blank">{{ githubText??'https://github.com/Baizidmdashadzzaman' }}</a>
+        <a :href="github" target="_blank">{{ github }}</a>
       </p>
 
-      <p>
+      <p v-if="cv">
         <strong>CV:</strong>
-        <a target="_blank" href="https://drive.google.com/file/d/1HuLXxOB6dL58AcupKZ9qhRr7739EXLML/view" >Download CV now</a>
+        <a target="_blank" :href="cv">Download CV now</a>
       </p>
 
-      <iframe src="https://baizidmdashadzzaman-next.vercel.app/" width="100%" height="1000"></iframe>
+      <iframe v-if="portfolio" :src="portfolio" width="100%" height="1000"></iframe>
 
     </div>
   </template>
@@ -32,12 +32,16 @@
       },
       github: {
         type: String,
-        default: '#'
+        default: ''
       },
-      githubText: {
+      cv: {
         type: String,
         default: ''
-      }
+      },
+      portfolio: {
+        type: String,
+        default: ''
+      },
     }
   };
   </script>
