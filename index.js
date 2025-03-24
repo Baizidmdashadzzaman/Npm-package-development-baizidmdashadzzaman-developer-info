bf102c6abae27c39dc9a295d8ad4a4f2afc8d6be
Baizidmdashadzzaman <baizid.md.ashadzzaman@gmail.com>
2025-03-24 19:00:47 +0600

    updated index,js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // index.js
-const DeveloperPlugin = require('./vue/DeveloperPlugin');
 const DeveloperPage = require('./react/DeveloperPage');
+const DeveloperPageAsad = require('./react/DeveloperPageAsad');
+
+const DeveloperPlugin = require('./vue/DeveloperPlugin');
+const DeveloperComponent = require('./vue/DeveloperComponent.vue');
+const DeveloperComponentAsad = require('./vue/DeveloperComponentAsad.vue');
+
 
 module.exports = {
   DeveloperPlugin,
+  DeveloperComponent,
+  DeveloperComponentAsad,
   DeveloperPage,
+  DeveloperPageAsad,
 };
